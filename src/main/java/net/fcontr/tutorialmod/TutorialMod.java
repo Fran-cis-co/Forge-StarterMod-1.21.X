83e30400e3793dd07b6397d5d9598ef898215e57
Fran-cis-co <fcontreras102301@gmail.com>
2024-12-09 11:39:37 -0800

    Added 2 new items to the game along with a creative tab with those items

--- src/main/java/net/fcontr/tutorialmod/TutorialMod.java
+++ src/main/java/net/fcontr/tutorialmod/TutorialMod.java
@@ -1,6 +1,10 @@
 package net.fcontr.tutorialmod;
 
 import com.mojang.logging.LogUtils;
+import net.fcontr.tutorialmod.item.ModCreativeModeTabs;
+import net.fcontr.tutorialmod.item.ModItems;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -25,6 +29,11 @@ public class TutorialMod {
 
     public TutorialMod() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModCreativeModeTabs.register(modEventBus);
+
+        ModItems.register(modEventBus);
+
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
         // Register ourselves for server and other game events we are interested in
@@ -43,7 +52,10 @@ public class TutorialMod {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS){
+            event.accept(ModItems.SAPPHIRE);
+            event.accept(ModItems.RAW_SAPPHIRE);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
---
+++ src/main/java/net/fcontr/tutorialmod/item/ModCreativeModeTabs.java
@@ -0,0 +1,34 @@
+package net.fcontr.tutorialmod.item;
+
+import net.fcontr.tutorialmod.TutorialMod;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
+
+// Java class which is used to program new creative mode tabs to store our modded items
+public class ModCreativeModeTabs {
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS =
+            DeferredRegister.create(Registries.CREATIVE_MODE_TAB, TutorialMod.MOD_ID);
+
+    public static final RegistryObject<CreativeModeTab> TUTORIAL_TAB = CREATIVE_MODE_TABS.register("tutorial_tab",
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.SAPPHIRE.get()))
+                    .title(Component.translatable("creativetab.tutorial_tab"))
+                    .displayItems((pParameters, pOutput) -> {
+                        // Adding custom items to the new creative mode tab
+                        pOutput.accept(ModItems.SAPPHIRE.get());
+                        pOutput.accept(ModItems.RAW_SAPPHIRE.get());
+
+                        // Adding vanilla items to the tab
+                        pOutput.accept(Items.DIAMOND);
+                    })
+                    .build());
+
+    public static void register(IEventBus eventBus){
+        CREATIVE_MODE_TABS.register(eventBus);
+    }
+}
